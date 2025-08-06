3a92cc3453cdce036f2a79a277a3c60d11a8d03b
FaridaBarkenkhoyeva <barkenkhoyeva.farida@outlook.com>
2025-08-06 19:28:30 +0200

    creating endpoint /newPost and adding posts into database

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ app.use(express.json());
 const dotenv = require("dotenv");
 dotenv.config();
 
-
 const cors = require("cors");
 app.use(cors());
 
@@ -22,47 +21,34 @@ async function addPost() {
       author: "Farida",
       title: "First post",
       content: "First post",
-      cover: "http://images.com"
+      cover: "http://images.com",
     },
   });
   try {
-    
     console.log("Post has been added");
   } catch (err) {
     console.error("Error adding a post");
     console.log(err.message);
   }
-
-  
 }
 
-addPost();
 
-// app.post("/newPost", async (req, res) => {
-//   try {
-
-//   // const post = await prisma.post.create({
-//   //   data: {
-//   //     author: "Milana",
-//   //     title: "First try",
-//   //     content: "Starting a new Post with this new exciting content!",
-//   //     cover: "http://image.com", }})
-
-//   }});
-// try {
-//   const { author, title, content, cover } = req.body;
-//   if (!author || !title || !content || !cover) {
-//     return res
-//       .status(400)
-//       .json({ error: "Author, title, content and cover are required" });
-//   }
-//   console.log("received data:", { author, title, content, cover });
-//   await addPost(author, title, content, cover);
-//   res.status(201).json({ message: "Post has been added successfully" });
-// } catch (err) {
-//   console.error("Error in /newPost:", err);
-//   res.status(500).json({ error: "Internal server error" });
-// }
+app.post("/newPost", async (req, res) => {
+  try {
+    const { author, title, content, cover } = req.body;
+    if (!author || !title || !content) {
+      return res
+        .status(400)
+        .json({ error: "Author, title and content are required" });
+    }
+    console.log("received data:", { author, title, content, cover });
+    await addPost(author, title, content, cover);
+    res.status(201).json({ message: "Post has been added successfully" });
+  } catch (err) {
+    console.error("Error in /newPost:", err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 const port = "3007";
 app.listen(port, (req, res) => {
